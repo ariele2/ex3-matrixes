677ddd702ec77308f7c4e9f3f37e0636d2bf1452
ariele2 <63596137+ariele2@users.noreply.github.com>
2020-07-01 12:49:11 +0300

    Add files via upload

--- Part C/Game.h
+++ Part C/Game.h
@@ -16,17 +16,28 @@ namespace mtm {
         Matrix<std::shared_ptr<mtm::Character>> board; 
         public:
         Game() = delete;
+        // constructor - recieves the height and width of the game board.
         Game(int height, int width);
         ~Game() = default;
+        // copy construcor - makes a deep copy of a game.
         Game (const Game& other);
+        // operator= - makes a deep copy of a game, to a game that already initialized.
         Game& operator=(const Game& other);
+        // makes a new character, receiving all the neccessary parameters of a character.
         static std::shared_ptr<Character> makeCharacter(CharacterType type, Team team,
                                         units_t health, units_t ammo, units_t range, units_t power);
+        // adds a premade character (with makeCharacter) to a coordinates entered by the user.
         void addCharacter(const GridPoint& coordinates, std::shared_ptr<Character> character);
-        void move(const GridPoint& src_coordinates, const GridPoint& dst_coordinates);  
+        // moves a character from a source coordinates to a destiny coordinates.
+        void move(const GridPoint& src_coordinates, const GridPoint& dst_coordinates); 
+        // attacks a character on the destiny coordinates, by a character on the source coordinates. 
         void attack(const GridPoint& src_coordinates, const GridPoint& dst_coordinates);
+        //reloads the ammo of the character on the entered coordinates.
         void reload(const GridPoint& coordinates);
+        // checks if one of the teams won, if so - returns true. also updates a pointer (if entered) that
+        // tells who is the winning team.
         bool isOver(Team* winningTeam=NULL) const;
+        // operator<< : use it to print the game board to the output terminal.
         friend std::ostream& operator<<(std::ostream& os,const Game& game){
             std::vector<char> board_vector;
             for (int i=0; i<game.height; i++) {
